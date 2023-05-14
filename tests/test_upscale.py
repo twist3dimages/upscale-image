2b2898fe5622b6778b025187790926c6c658fbb7
Charley <33134502+twist3dimages@users.noreply.github.com>
2023-05-14 12:58:42 -0400

    Create test_upscale.py

---
+++ tests/test_upscale.py
@@ -0,0 +1,10 @@
+﻿import pytest
+from upscale import upscale_images_in_dir
+
+def test_upscale_images_in_dir():
+    # Placeholder test to verify script execution without errors
+    upscale_images_in_dir(None, None, None)
+    assert True
+
+if __name__ == '__main__':
+    pytest.main(['-v', '-s'])
